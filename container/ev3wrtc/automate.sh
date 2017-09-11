5db1c2da1eb2b41af3f4c3c3886ea49c51fad10a
Jesko Appelfeller <jesko@appelfeller.eu>
2017-09-11 12:24:39 +0200

    make cmake install files into nfs share

--- container/ev3wrtc/automate.sh
+++ container/ev3wrtc/automate.sh
@@ -3,5 +3,6 @@
 #Script to automate building of the WebRTCDemonstrator robot software and optionally copy the result to the robot.
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_INSTALL_PREFIX=$PWD ..
 make
+make install
