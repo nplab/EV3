7bf8750102523372def6bf3e5d9b12029bc65ce0
Jesko Appelfeller <jesko@appelfeller.eu>
2018-03-01 13:41:13 +0100

    add functions for traversal of config tree

--- robot/src/config.c
+++ robot/src/config.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 #include "config.h"
 #include "utils.h"
 
-static cJSON *root = NULL;
+static cJSON *conf_root = NULL;
 
 int read_config(char *filename){
   //open file
@@ -21,6 +22,16 @@ int read_config(char *filename){
     handle_errno("Could not read config file", true);
   }
 
+  //parse config
+  conf_root = cJSON_Parse(conf_string);
+  if( conf_root == NULL){
+    const char *problem = cJSON_GetErrorPtr();
+    if( problem != NULL ){
+      fprintf(stderr, "Parsing of JSON in config file failed  before: %s\n", problem);
+      abort();
+    }
+  }
+
   //clean up
   fclose(fd);
   free(conf_string);
@@ -29,14 +40,38 @@ int read_config(char *filename){
 }
 
 int delete_config(){
-  cJSON_Delete(root);
+  cJSON_Delete(conf_root);
   return WRTCR_SUCCESS;
+}
+
+cJSON* get_conf_item(char *key){
+  cJSON *cur_item = conf_root;
+  char *cur_token = strtok(key, '.');
+  while(cur_token != NULL){
+    cur_item = cJSON_getObjectItem(cur_item, cur_token);
+    char *cur_token = strtok(key, '.');
+  }
+  return cur_item;
 }
 
 int get_int(char *key, int *value){
-  return WRTCR_SUCCESS;
+  cJSON *item = get_conf_item(key);
+  if( item == NULL || !cJSON_IsNumber(item)){
+    return WRTCR_FAILURE;
+  }
+  else{
+    *value = item->valueint;
+    return WRTCR_SUCCESS;
+  }
 }
 
-int get_string(char *key, char *value){
-  return WRTCR_SUCCESS;
+int get_string(char *key, char **value){
+  cJSON *item = get_conf_item(key);
+  if( item == NULL || !cJSON_IsString(item)){
+    return WRTCR_FAILURE;
+  }
+  else{
+    *value = item->valuestring;
+    return WRTCR_SUCCESS;
+  }
 }
