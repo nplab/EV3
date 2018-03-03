8f89434bac06cdce5511025cbdbd0a0a902b9f13
Jesko Appelfeller <jesko@appelfeller.eu>
2018-03-03 23:09:43 +0100

    enable cmake to find the rawrtc

--- container/build_ev3wrtc.sh
+++ container/build_ev3wrtc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -t ev3wrtc:latest -t git.fh-muenster.de:4567/ja753563/webrtcdemonstrator/ev3wrtc:latest https://git.fh-muenster.de/ja753563/WebRTCDemonstrator.git\#:container/ev3wrtc
+sudo docker build -t ev3wrtc:latest -t git.fh-muenster.de:4567/ja753563/webrtcdemonstrator/ev3wrtc:latest .
