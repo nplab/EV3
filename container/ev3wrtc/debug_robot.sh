f30f4c52ac01d29771f653d5b05b8247ef4c39ae
Jesko Appelfeller <jesko@appelfeller.eu>
2018-03-16 08:39:27 +0100

    make gdbserver keep running

--- container/ev3wrtc/debug_robot.sh
+++ container/ev3wrtc/debug_robot.sh
@@ -1,1 +1,1 @@
-gdb-multiarch --tui --quiet --directory=./src --symbols=./bin/wrtcrobot --eval-command="target remote $GDBREMOTE"
+gdb-multiarch --tui --quiet --directory=./src --symbols=./bin/wrtcrobot --eval-command="target extended-remote $GDBREMOTE"
