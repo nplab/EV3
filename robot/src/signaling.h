ab45394a45f7332eac35cf9e58f3d7b0aa374373
Jesko Appelfeller <jesko@appelfeller.eu>
2018-03-17 15:12:03 +0100

    make signaling use return codes

--- robot/src/signaling.h
+++ robot/src/signaling.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#define _BSD_SOURCE
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netdb.h>
+
+#include "utils.h"
+#include "config.h"
+
 //connect to signaling server based on config file
-int sigserv_connect();
+wrtcr_rc sigserv_connect();
 
 //send a string to the signaling server in the current room
-int sigserv_send(char *msg);
+wrtcr_rc sigserv_send(char *msg);
 
 //receive a string message from the signaling server in the current room
-int sigserv_receive(char **msg);
+wrtcr_rc sigserv_receive(char **msg);
 
 //disconnect from the signaling server
-int sigserv_disconnect();
+wrtcr_rc sigserv_disconnect();
