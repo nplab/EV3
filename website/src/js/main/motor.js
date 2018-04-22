3c7901d168f6c9d746b2936bff230d18f1704bc4
Frederik Broer <frederik.Broer@fh-muenster.de>
2018-04-22 20:01:57 +0200

    add comments

--- website/src/js/main/motor.js
+++ website/src/js/main/motor.js
@@ -1,23 +1,31 @@
+/*
+In dieser Klasse sind alle notwendigen Funktionen definiert, damit ein Lego Minestrom Motor gesteuert werden kann.
+
+Dafür ist es notwenig, dass sich die Weboberfläche mit dem Roboter verbindet (z.B. mittels webRTC). Außerdem muss anschließend die Funktion 'sendingToRoboter' definiert werden, sodass der Roboter die Nachrichten erhalten kann.
+
+*/
+
 class Motor {
     constructor(port, type, mode = null) {
-        this.port = port;
-        this.type = type;
-        this.modes = null;
-        this.selectedMode = mode;
+        this.port = port;               // Port des Motors
+        this.type = type;               // Typ des Motors
+        this.modes = null;              // Liste der wählbaren Modes
+        this.selectedMode = mode;       // Ausgewählter Modus [Wird nicht überprüft]
         this.value = null;
 
         // Set List of modes
         this.getModes()
     }
+
+    // Je nach Typ des Sensors können unterschiedliche Modes genutzt werden. Zurzeit werden für beide Motoren die beiden gleichen Modi unterstützt.
     getModes() {
         var mode1 = 'run-forever';
         var mode2 = 'run-to-rel-position';
 
         this.modes = [mode1, mode2]
     }
-    setSelectedMode(mode) {
-        this.selectedMode = mode;
-    }
+
+    // Setter value
     setValue(value) {
         if (this.checkValue(value)) {
             this.value = value
@@ -25,6 +33,8 @@ class Motor {
         }
         return false
     }
+
+    // Der Wert muss überprüft werden, weil der Roboter in den verschiedenen Modie nur unterschiedliche Werte erlauben. Zurzeit werden nur zwei Modie unterstützt.
     checkValue(value) {
         if (this.selectedMode == "run-forever") {
             if(value <= 100 && value >= 0) {
@@ -40,9 +50,13 @@ class Motor {
             return false
         }
     }
+
+    // Fahren des Roboters. Es wird eine Richtung ('direction') definiert. "+" -> Vorwärts; "-" -> Rückwärts
     drive(direction) {
         sendingToRoboter(this.port, this.selectedMode, this.value * direction);
     }
+
+    // Stoppen des Motors
     stop() {
         sendingToRoboter(this.port, 'stop')
     }
