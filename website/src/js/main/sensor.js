65b9c8086ecd191c745a2ad9ee79b8945081b41d
Frederik Broer <frederik.Broer@fh-muenster.de>
2018-05-23 00:12:29 +0200

    define function 'setvalue' for saving values

--- website/src/js/main/sensor.js
+++ website/src/js/main/sensor.js
@@ -10,7 +10,7 @@ class Sensor {
         this.type = type;           // Typ des Sensors
         this.modes = null;          // Liste der wählbaren Modes
         this.selectedMode = mode;   // Ausgewählter Modus
-        this.value = null;          //
+        this.value = null;          // Value Wert
 
         // Set List of modes
         this.getModes()
@@ -67,6 +67,8 @@ class Sensor {
     }
 
     // Speichern des Wertes, Werte
-
+    setValue(value) {
+        this.value = value
+    }
     // Funktion für neue Werte
 }
